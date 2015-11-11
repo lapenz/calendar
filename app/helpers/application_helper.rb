c39003447c22fdb986a48c504cd04a92b148a1a3
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2015-11-10 23:41:34 -0200

    Confgurando bootstrap.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def nav_link(link_text, link_path, http_method=nil)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, class: class_name) do
+      if http_method
+        link_to(link_text, link_path, method: http_method)
+      else
+        link_to(link_text, link_path)
+      end
+    end
+  end
 end

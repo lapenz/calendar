f6ba7c89009339269df9c9cd35f5e0e018bb1209
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2018-05-04 10:44:57 -0300

    Format duration of companies services.

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -1,2 +1,15 @@
 module AppointmentsHelper
+
+  # Will take as input a time in seconds (which is typically a result after subtracting two Time objects),
+  # and return the result in HH:MM:SS, even if it exceeds a 24 hour period.
+  def formatted_duration(total_seconds)
+    hours = total_seconds / (60 * 60)
+    minutes = (total_seconds / 60) % 60
+    seconds = total_seconds % 60
+    [hours, minutes, seconds].map do |t|
+      # Right justify and pad with 0 until length is 2.
+      # So if the duration of any of the time components is 0, then it will display as 00
+      t.round.to_s.rjust(2,'0')
+    end.join(':')
+  end
 end

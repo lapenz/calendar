ce7734b8f22f208c53c32ba5f9b5b69d9c22a064
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2018-05-16 10:22:42 -0300

    Internacionalizacao da parte administrativa.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,6 @@
 class AppointmentsController < ApplicationController
   before_action :set_appointment, only: [:show, :edit, :update, :destroy, :resume]
-
+  before_action :authenticate_user!
   helper CompaniesHelper
 
   # GET /appointments

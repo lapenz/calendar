5ab769224ea5c900c7abf38624d121f4f8bc691e
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2018-06-11 20:59:26 -0300

    Melhorando o cadastro de usuario.

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,7 +5,15 @@ class Company < ActiveRecord::Base
   has_many :clients
 
   validates_uniqueness_of :minipage_url
+  validates_presence_of :name
 
   mount_uploader :logo, LogoUploader
+
+  before_validation :set_minipage_url, on: :create
+
+  private
+  def set_minipage_url
+    self.minipage_url = self.name.downcase.delete(' ') + rand(10).to_s
+  end
 
 end

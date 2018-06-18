bfd76374f9e385457d8cf60dd6b61231d4939698
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2018-06-18 18:31:35 -0300

    Corrigindo npe no CompaniesHelper.

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,11 +1,13 @@
 module CompaniesHelper
 
   def address_url(company)
-    url = company.street.downcase + ' ' + company.city.downcase
-    URI.encode(url, /\W/)
+    url = company.street.downcase unless company.street.blank?
+    url += ' ' + company.city.downcase unless company.city.blank?
+    URI.encode(url, /\W/) unless url.blank?
   end
 
   def address(company)
-    company.street.titleize + ', ' + company.city.titleize
+    text = company.street.titleize unless company.street.blank?
+    text += ', ' + company.city.titleize unless company.city.blank?
   end
 end

72187804a0e82f78f8ec1947a63d44ebc1f103e4
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2018-06-19 10:29:04 -0300

    Removendo unique do email de client.

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,8 +4,6 @@ class Client < ActiveRecord::Base
 
   attr_accessor :full_validate
 
-  validates :email, uniqueness: { scope: :company_id }
-
   validates_presence_of :name
 
   validates_presence_of :email, :phone, :if => 'full_validate.present?'

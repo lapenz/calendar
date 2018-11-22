b74553d8881dab95eb4dbcac7cc73b04771ab5fc
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2018-11-22 09:44:17 -0200

    Alterando migrate de index do clients.

--- db/migrate/20180528143307_add_index_to_clients.rb
+++ db/migrate/20180528143307_add_index_to_clients.rb
@@ -1,6 +1,5 @@
 class AddIndexToClients < ActiveRecord::Migration
   def change
-    remove_index :clients, :email
-    add_index :clients, [:email, :company_id], unique: true
+     add_index :clients, [:email, :company_id], unique: true
   end
 end

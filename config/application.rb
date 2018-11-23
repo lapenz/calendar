a12c574a55c64e60150652b5e221cc6f80438c86
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2018-11-23 09:18:53 -0200

    update application.rb

--- config/application.rb
+++ config/application.rb
@@ -31,9 +31,5 @@ module Calendar
     # Whitelist locales available for the application
     I18n.available_locales = [:en, :pt]
 
-    config.assets.initialize_on_precompile = false
-
-    config.serve_static_files = true
-
   end
 end

c3f900ca8215d8a4d289327b65d888869da29301
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2018-11-29 15:17:44 -0200

    Config secretkey.

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,6 +26,8 @@ append :linked_files, "config/database.yml"
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
+append :linked_files, "config/master.key"
+
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 

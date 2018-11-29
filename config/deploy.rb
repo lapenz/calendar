744495af0bf24392dfd0f7d6349eb9bf9fe17247
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2018-11-29 14:59:58 -0200

    Subindo ruby 2.5.0 para 2.5.3

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ set :keep_releases, 5
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 #
-set :rvm_ruby_version, '2.2.6'
+set :rvm_ruby_version, '2.5.3'
 
 set :tmp_dir, "/home/ec2-user"
 

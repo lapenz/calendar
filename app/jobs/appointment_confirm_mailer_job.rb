c3dfda8dd93495577946b88e0767587c684cd111
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2018-12-10 14:30:02 -0200

    Fixing confirm mailer

--- app/jobs/appointment_confirm_mailer_job.rb
+++ app/jobs/appointment_confirm_mailer_job.rb
@@ -2,7 +2,7 @@ class AppointmentConfirmMailerJob < ApplicationJob
   queue_as :default
 
   def perform(appointment, type)
-    AppointmentsMailer.confirm_appointment(appointment, type).deliver_now unless appointment.client.email.blank?
+    AppointmentsMailer.confirm_appointment(appointment, type).deliver_now if appointment.client.email.present?
 
   end
 end

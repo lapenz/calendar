87c3b552ce23b96a0618353199a74a81e5c098f5
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2020-09-26 21:46:47 -0500

    Setting up some keys

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,12 @@ module Calendar
     I18n.available_locales = [:en, :pt]
 
     config.active_job.queue_adapter = :sidekiq
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
